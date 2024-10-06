9b3392d63b30d90be086d6e9a6d7744d6a8ba1b2
xxb-cyber <3462973218@qq.com>
2024-10-06 17:15:19 +0800

    Delete js directory

--- js/top_button.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('top_button').addEventListener('click', function(event) {
-    event.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
